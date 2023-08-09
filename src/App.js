65f57d23a7d619462c3a295979d5d2041331f402
kumaresan-mitrah <kumaresan.r@mitrahsoft.com>
2023-08-09 19:13:10 +0530

    Added the changes that mentioned in the pr comments

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, lazy } from 'react';
 import {
   Routes,
   Route,
@@ -9,43 +9,43 @@ import { PrivateRoute } from './services/router/privateRoute';
 import { PublicRoute } from './services/router/publicRoute';
 import { HOME_PATH, INDEX_PATH, LOGIN_PATH } from './constants/route';
 
-const Login = React.lazy(() => import('./pages/login'));
-const Layout = React.lazy(() => import('./pages/layout'));
+const Login = lazy(() => import('./pages/login'));
+const Layout = lazy(() => import('./pages/layout'));
 
 function App() {
   return (
-    <div>
-      <Suspense fallback={<div className='d-flex justify-content-center mt-5'>{<Spin />}</div>}>
-        <Routes>
-          <Route
-            path={LOGIN_PATH}
-            element={
-              <PublicRoute
-                component={<Login />}
-                redirectedUrl={HOME_PATH}
-              />
-            }
-          />
-          <Route
-            path={INDEX_PATH}
-            element={
-              <PrivateRoute
-                component={<Layout />}
-                redirectedUrl={LOGIN_PATH}
-              />
-            }
-          />
-          <Route
-            path="*"
-            element={
-              <div className='d-flex justify-content-center mt-5'>
-                No Page Found
-              </div>
-            }
-          />
-        </Routes>
-      </Suspense>
-    </div>
+    // Todo: Need to update this fallback component with our blockUI component once its ready.
+    <Suspense
+      fallback={<div className='d-flex justify-content-center mt-5'>{<Spin />}</div>}>
+      <Routes>
+        <Route
+          path={LOGIN_PATH}
+          element={
+            <PublicRoute
+              component={<Login />}
+              redirectedUrl={HOME_PATH}
+            />
+          }
+        />
+        <Route
+          path={INDEX_PATH}
+          element={
+            <PrivateRoute
+              component={<Layout />}
+              redirectedUrl={LOGIN_PATH}
+            />
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <div className='d-flex justify-content-center mt-5'>
+              No Page Found
+            </div>
+          }
+        />
+      </Routes>
+    </Suspense>
   );
 }
 
