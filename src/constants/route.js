521d4c13c079935aab5d3bb66c037e9215d5758c
Alex K.D <alex.d@mitrahsoft.com>
2023-08-09 13:42:22 +0530

    Added husky and prettier

--- src/constants/route.js
+++ src/constants/route.js
@@ -1,7 +1,7 @@
 export const LOGIN_PATH = "/login";
 
 export const INDEX_PATH = "/*";
-export const ACCESS_DENIED = "/noaccess";
+export const ACCESS_DENIED = "/no-access";
 
 export const HOME_PATH = "/";
 export const DASHBOARD_PATH = "/dashboard";
--- src/pages/layout/content/index.jsx
+++ src/pages/layout/content/index.jsx
@@ -1,39 +1,37 @@
 import { Layout } from 'antd';
 import { Routes, Route } from 'react-router-dom';
 
-import { DASHBOARD_PATH, HOME_PATH, SETTINGS_PATH } from '../../../constants/route';
+import { DASHBOARD_PATH, HOME_PATH, SETTINGS_PATH } from '../../../constants';
 import Dashboard from '../../dashboard/index';
 import { Home } from '../../home';
 import Settings from '../../settings/index';
 const { Content } = Layout;
 
 export default function CustomContent() {
+  const routeSources = [
+    {
+      path: HOME_PATH,
+      component: <Home />
+    },
+    {
+      path: DASHBOARD_PATH,
+      component: <Dashboard />
+    },
 
-    const routeSources = [
-        {
-            path: HOME_PATH,
-            component: <Home />,
-        },
-        {
-            path: DASHBOARD_PATH,
-            component: <Dashboard />
-        },
+    {
+      path: SETTINGS_PATH,
+      component: <Settings />
+    }
+  ];
 
-        {
-            path: SETTINGS_PATH,
-            component: <Settings />
-        }
-    ];
-
-
-    return (
-        <Content style={{ margin: '24px 16px 0' }}>
-            <Routes>
-                {routeSources.map(({ path, component: Component }) => (
-                    <Route key={path} path={path} element={Component} />
-                ))}
-                <Route path={'*'} element={<div>No path found!!</div>} />
-            </Routes>
-        </Content>
-    )
+  return (
+    <Content style={{ margin: '24px 16px 0' }}>
+      <Routes>
+        {routeSources.map(({ path, component: Component }) => (
+          <Route key={path} path={path} element={Component} />
+        ))}
+        <Route path={'*'} element={<div>No path found!!</div>} />
+      </Routes>
+    </Content>
+  );
 }
