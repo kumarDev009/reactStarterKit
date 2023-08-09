4093f33223f5fe5aa534c9ebbc8b0ed156061374
kumaresan-mitrah <kumaresan.r@mitrahsoft.com>
2023-08-09 16:01:11 +0530

    Added the lazy loading with suspense fallback

--- src/App.js
+++ src/App.js
@@ -1,46 +1,50 @@
-
-import Login from './pages/login';
+import React, { Suspense } from 'react';
 import {
   Routes,
   Route,
 } from 'react-router-dom';
+import { Spin } from 'antd';
 
 import { PrivateRoute } from './services/router/privateRoute';
 import { PublicRoute } from './services/router/publicRoute';
 import { HOME_PATH, INDEX_PATH, LOGIN_PATH } from './constants/route';
-import Layout from './pages/layout';
+
+const Login = React.lazy(() => import('./pages/login'));
+const Layout = React.lazy(() => import('./pages/layout'));
 
 function App() {
-
   return (
     <div>
-      <Routes>
-        <Route
-          path={LOGIN_PATH}
-          element={
-            <PublicRoute
-              component={<Login />}
-              redirectedUrl={HOME_PATH}
-            />
-          }
-        />
-        <Route
-          path={INDEX_PATH}
-          element={
-            <PrivateRoute
-              component={<Layout />}
-              redirectedUrl={LOGIN_PATH}
-            />
-          }
-        />
-        <Route
-          path="*"
-          element={
-            <div className='d-flex justify-content-center mt-5'>
-              No Page Found
-            </div>}>
-        </Route>
-      </Routes>
+      <Suspense fallback={<div className='d-flex justify-content-center mt-5'>{<Spin />}</div>}>
+        <Routes>
+          <Route
+            path={LOGIN_PATH}
+            element={
+              <PublicRoute
+                component={<Login />}
+                redirectedUrl={HOME_PATH}
+              />
+            }
+          />
+          <Route
+            path={INDEX_PATH}
+            element={
+              <PrivateRoute
+                component={<Layout />}
+                redirectedUrl={LOGIN_PATH}
+              />
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <div className='d-flex justify-content-center mt-5'>
+                No Page Found
+              </div>
+            }
+          />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
