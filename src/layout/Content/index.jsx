3d28951d2203d477685267ff516557ab72551388
kumaresan-mitrah <kumaresan.r@mitrahsoft.com>
2023-08-30 12:26:38 +0530

    Feature#28: Added the page-not-found component in the route configuration

--- src/layout/Content/index.jsx
+++ src/layout/Content/index.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import { DASHBOARD_PATH, HOME_PATH, SETTINGS_PATH, KITCHEN_SINK_PATH } from 'constants/route';
 import ErrorBoundary from 'components/ErrorBoundary';
+import NotFound from 'pages/notFound';
 
 const { Content } = Layout;
 
@@ -36,8 +37,7 @@ export default function CustomContent() {
             {routeSources.map(({ path, component: Component }) => (
               <Route key={path} path={path} element={<Component />} />
             ))}
-            {/* TODO: Design our own or show antd result for not found page */}
-            <Route path={'*'} element={<div>No path found!!</div>} />
+            <Route path={'*'} element={<NotFound />} />
           </Routes>
         </Content>
       </Suspense>
