f3ab1cdad5e310d3197adf86bb373cc28a152ce3
kumaresan-mitrah <kumaresan.r@mitrahsoft.com>
2023-09-26 12:16:16 +0530

    Feature:25:CleanUp the code

--- src/constants/themeConfig.js
+++ src/constants/themeConfig.js
@@ -4,23 +4,40 @@ import { DARK, DARK_PRIMARY_BG, LIGHT, LIGHT_PRIMARY_BG } from './theme';
 
 const { defaultAlgorithm, darkAlgorithm, compactAlgorithm } = theme;
 
+const THEME_CONFIG = {
+  token: {
+    colorPrimaryBg: { DARK: DARK_PRIMARY_BG, LIGHT: LIGHT_PRIMARY_BG }
+  },
+  components: {
+    Menu: {
+      colorPrimary: { DARK: LIGHT, LIGHT: DARK }
+    },
+    Layout: {
+      colorBgHeader: { DARK: DARK, LIGHT: LIGHT },
+      colorTextLightSolid: { DARK: LIGHT, LIGHT: DARK }
+    },
+    Button: {
+      colorLinkHover: { DARK: DARK_PRIMARY_BG, LIGHT: DARK_PRIMARY_BG }
+    }
+  },
+  algorithm: { DARK: darkAlgorithm, LIGHT: defaultAlgorithm }
+};
+
+const mappedConfig = (obj, mode) => {
+  let result = {};
+  for (const key in obj) {
+    if (obj[key][mode]) {
+      result[key] = obj[key][mode];
+    } else if (typeof obj[key] === 'object') {
+      result[key] = mappedConfig(obj[key], mode);
+    }
+  }
+  return result;
+};
+
 export const getThemeConfig = isDarkMode => {
-  return {
-    token: {
-      colorPrimaryBg: isDarkMode ? DARK_PRIMARY_BG : LIGHT_PRIMARY_BG
-    },
-    components: {
-      Menu: {
-        colorPrimary: isDarkMode ? LIGHT : DARK
-      },
-      Layout: {
-        colorBgHeader: isDarkMode ? DARK : LIGHT,
-        colorTextLightSolid: isDarkMode ? LIGHT : DARK
-      },
-      Button: {
-        colorLinkHover: DARK_PRIMARY_BG
-      }
-    },
-    algorithm: [isDarkMode ? darkAlgorithm : defaultAlgorithm, compactAlgorithm]
-  };
+  const mode = isDarkMode ? 'DARK' : 'LIGHT';
+  const mappedTheme = mappedConfig(THEME_CONFIG, mode);
+  mappedTheme.algorithm = [mappedTheme.algorithm, compactAlgorithm];
+  return mappedTheme;
 };
