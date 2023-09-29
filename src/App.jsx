2992ad2704e034cd724e1a786ec4e0513c121176
kumaresan-mitrah <kumaresan.r@mitrahsoft.com>
2023-09-29 12:43:56 +0530

    Feature#61:Fixed the bug that locale not reflecting on the login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import i18n from 'i18n';
 
 import Routers from 'routers';
 import AuthContextProvider from 'context/authContext';
 import './index.scss';
 
 function App() {
+  useEffect(() => {
+    i18n.on('initialized');
+  }, []);
+
   return (
     <BrowserRouter>
       <AuthContextProvider>
