cced4507456fc876ca01c05fa5f7601fbb28c797
Meena M <meena.m@mitrahsoft.com>
2023-10-10 18:09:34 +0530

    feature: #69-Integreated the api in forgot-password page

--- src/services/query/auth/index.js
+++ src/services/query/auth/index.js
@@ -1,5 +1,5 @@
 import { useMutation } from 'react-query';
-import { registerUser } from 'services/api/auth';
+import { registerUser, generateOtp, newPasswordUpdate, verifyOtp } from 'services/api/auth';
 
 export const useRegister = () => {
   return useMutation(registerUser, {
@@ -9,6 +9,45 @@ export const useRegister = () => {
     onSuccess: data => {
       if (data) {
         console.log('Registration successful:', data);
+      }
+    }
+  });
+};
+
+export const useGenerateOtp = () => {
+  return useMutation(generateOtp, {
+    onError: error => {
+      console.log('Error in generateOTP', error);
+    },
+    onSuccess: data => {
+      if (data) {
+        console.log('generate OTP Successfully', data);
+      }
+    }
+  });
+};
+
+export const useVerifyOtp = () => {
+  return useMutation(verifyOtp, {
+    onError: error => {
+      console.log('Error in otp verification', error);
+    },
+    onSuccess: data => {
+      if (data) {
+        console.log('OTP verification successfully', data);
+      }
+    }
+  });
+};
+
+export const usePasswordUpdate = () => {
+  return useMutation(newPasswordUpdate, {
+    onError: error => {
+      console.log('Error in new password update', error);
+    },
+    onSuccess: data => {
+      if (data) {
+        console.log('Successfully updated the new password', data);
       }
     }
   });
